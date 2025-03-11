914f14cae29265284df709d07f71f75216130ebb
Joel Singh <joelsingh788@gmail.com>
2025-03-11 12:21:30 -0400

    Add <> snippet

--- lua/custom/snippets/all.lua
+++ lua/custom/snippets/all.lua
@@ -33,4 +33,5 @@ return {
   s({ trig = '{}', snippetType = 'autosnippet', wordTrig = false }, fmta([[{<>}]], { i(1) })),
   s({ trig = '[]', snippetType = 'autosnippet', wordTrig = false }, fmta("[<>]", { i(1) })),
   s({ trig = '||', snippetType = 'autosnippet', wordTrig = false }, fmta("|<>|", { i(1) })),
+  s({ trig = '<>', snippetType = 'autosnippet', wordTrig = false }, fmt("<{}>", { i(1) })),
 }
